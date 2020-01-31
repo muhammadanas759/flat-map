1858def6b862c1ec21fb3b35d5c3e840541ee1fa
deadsmond <deadsmond@users.noreply.github.com>
2020-01-31 19:49:40 +0100

    icons  for markers

--- lib/resources/objects/map/google_maps_widget.dart
+++ lib/resources/objects/map/google_maps_widget.dart
@@ -7,6 +7,8 @@ import 'utils/map_marker.dart';
 import 'utils/map_helper.dart';
 
 import '../widgets/text_styles.dart';
+import '../data/icon_loader.dart';
+import '../data/marker_loader.dart';
 
 
 class GoogleMapWidget extends StatefulWidget {
@@ -39,68 +41,25 @@ class _GoogleMapWidgetState extends State<GoogleMapWidget> {
   /// Markers loading flag
   bool _areMarkersLoading = true;
 
-  // list of marker data
-  final List<Map> markersMap = [
-    {'position': LatLng(41.147125, -8.611249), 'icon': 'home'},
-    {'position': LatLng(41.145599, -8.610691), 'icon': 'pointer'},
-    {'position': LatLng(41.145645, -8.614761), 'icon': 'taxi'},
-    {'position': LatLng(41.146775, -8.614913), 'icon': 'water'},
-    {'position': LatLng(41.146982, -8.615682), 'icon': 'parliament'},
-    {'position': LatLng(41.140558, -8.611530), 'icon': 'tram'},
-    {'position': LatLng(41.138393, -8.608642), 'icon': 'pin'},
-    {'position': LatLng(41.137860, -8.609211), 'icon': 'factory'},
-    {'position': LatLng(41.138344, -8.611236), 'icon': 'meal'},
-    {'position': LatLng(41.139813, -8.609381), 'icon': 'biohazard'},
-  ];
-
-  /// Map of icon images urls used on normal markers
-  final Map markerImageUrl = {
-    'pointer':       'https://img.icons8.com/office/80/000000/marker.png',
-    'pointer_place': 'https://img.icons8.com/officel/80/000000/place-marker.png',
-    'pin':           'https://img.icons8.com/office/80/000000/map-pin.png',
-
-    'user':          'https://img.icons8.com/office/80/000000/street-view.png',
-    'clock':         'https://img.icons8.com/color/48/000000/clock--v1.png',
-    'post':          'https://img.icons8.com/office/80/000000/new-post.png',
-    'party':         'https://img.icons8.com/office/80/000000/party-baloons.png',
-    'gps':           'https://img.icons8.com/office/80/000000/gps-device.png',
-
-    'home':          'https://img.icons8.com/dusk/80/000000/order-delivered.png',
-    'office':        'https://img.icons8.com/office/80/000000/link-company-parent.png',
-    'factory':       'https://img.icons8.com/office/80/000000/factory.png',
-    'religion':      'https://img.icons8.com/office/80/000000/city-church.png',
-    'statue':        'https://img.icons8.com/office/80/000000/statue.png',
-    'parliament':    'https://img.icons8.com/office/80/000000/parliament.png',
-    'buildings':     'https://img.icons8.com/office/80/000000/city-buildings.png',
-
-    'tram':          'https://img.icons8.com/office/80/000000/tram.png',
-    'taxi':          'https://img.icons8.com/office/80/000000/taxi.png',
-    'train':         'https://img.icons8.com/office/80/000000/train.png',
-    'subway':        'https://img.icons8.com/office/80/000000/subway.png',
-    'car':           'https://img.icons8.com/office/80/000000/car.png',
-
-    'no_entry':      'https://img.icons8.com/office/80/000000/no-entry.png',
-    'no_mobile':     'https://img.icons8.com/office/80/000000/no-mobile-devices.png',
-
-    'biohazard':     'https://img.icons8.com/office/80/000000/biohazard.png',
-    'radioactive':   'https://img.icons8.com/office/80/000000/radio-active.png',
-    'water':         'https://img.icons8.com/office/80/000000/water.png',
-    'meal':          'https://img.icons8.com/office/80/000000/meal.png',
-  };
-
   /// Init [Fluster] and all the markers with network images and updates the loading state.
   void _initMarkers() async {
     final List<MapMarker> markers = [];
 
-    for (Map markerMap in markersMap) {
+    MarkerLoader _markerLoader = MarkerLoader();
+    _markerLoader.init();
+    final List<Map> _tempMarkers = _markerLoader.getList();
+
+    final IconLoader _iconLoader = IconLoader();
+
+    for (Map markerMap in _tempMarkers) {
       final BitmapDescriptor markerImage =
           await MapHelper.getMarkerImageFromUrl(
-              markerImageUrl[markerMap['icon']]
+              _iconLoader.dictionaryLocal[markerMap['icon']]
           );
 
       markers.add(
         MapMarker(
-          id: markersMap.indexOf(markerMap).toString(),
+          id: _tempMarkers.indexOf(markerMap).toString(),
           position: markerMap['position'],
           icon: markerImage,
         ),
@@ -111,7 +70,7 @@ class _GoogleMapWidgetState extends State<GoogleMapWidget> {
       markers,
       _minClusterZoom,
       _maxClusterZoom,
-      markerImageUrl['pointer_place'],
+      _iconLoader.dictionaryLocal['pointer_place'],
     );
 
     _updateMarkers();
