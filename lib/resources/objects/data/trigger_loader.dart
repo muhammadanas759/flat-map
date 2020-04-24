4460b22203e889896e4b56bf3629c2c3781ede91
deadsmond <deadsmond@users.noreply.github.com>
2020-04-24 15:12:04 +0200

    gelocator introduced

--- lib/resources/objects/data/trigger_loader.dart
+++ lib/resources/objects/data/trigger_loader.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:flatmapp/resources/extensions.dart';
 
 import 'package:volume/volume.dart';
 
@@ -9,6 +10,7 @@ import 'package:volume/volume.dart';
 class TriggerLoader {
   // ===========================================================================
   // init variables
+  // geolocator API: https://pub.dev/documentation/geolocator/latest/geolocator/Geolocator-class.html
   Geolocator _geolocator = Geolocator();
   LocationOptions locationOptions = LocationOptions(
       accuracy: LocationAccuracy.high,
@@ -19,31 +21,75 @@ class TriggerLoader {
 
   TriggerLoader() {
 
-    // check permission
-    Geolocator().checkGeolocationPermissionStatus().then((permission){
-      // TODO check permission status
-    });
-
-    positionStream = _geolocator.getPositionStream(locationOptions).listen(
-      (Position position){operatePositionChange(position: position);}
-    );
+//    // check permission
+//    Geolocator().checkGeolocationPermissionStatus().then((permission){
+//      // TODO check permission status
+//      print(permission);
+//    });
+//
+//    positionStream = _geolocator.getPositionStream(locationOptions).listen(
+//      (Position position){operatePositionChange(position: position);}
+//    );
   }
 
-  Future<List<Placemark>> placemarkFromAddress({String address}) async {
+  Future<LatLng> getCurrentPosition() async {
+    Position temp = await _geolocator.getCurrentPosition();
+    return temp.toLatLng();
+  }
+
+  Future<LatLng> getPositionFromAddress({String address}) async {
     // address type: Gronausestraat 710, Enschede
-    return await _geolocator.placemarkFromAddress(address);
+    List<Placemark> tempPlaceList = await _geolocator.placemarkFromAddress(address);
+
+    if (tempPlaceList != null && tempPlaceList.isNotEmpty) {
+      // this is all you need
+      Placemark placeMark  = tempPlaceList[0];
+      return placeMark.position.toLatLng();
+    } else {
+      return null;
+    }
   }
 
-  Future<List<Placemark>> placemarkFromCoordinates({LatLng position}) async {
-    return await _geolocator.placemarkFromCoordinates(
-        position.longitude, position.latitude
+  Future<String> getAddressFromPosition({LatLng position}) async {
+
+    List<Placemark> tempPlaceList = await _geolocator.placemarkFromCoordinates(
+        position.latitude, position.longitude
+    );
+
+    if (tempPlaceList != null && tempPlaceList.isNotEmpty) {
+      // this is all you need
+      Placemark placeMark  = tempPlaceList[0];
+      String name = placeMark.name;
+      String subLocality = placeMark.subLocality;
+      String locality = placeMark.locality;
+      String administrativeArea = placeMark.administrativeArea;
+      String postalCode = placeMark.postalCode;
+      String country = placeMark.country;
+      return "$subLocality $name, $locality, $administrativeArea $postalCode, $country";
+    } else {
+      return null;
+    }
+  }
+
+  Future<double> getDistanceBetweenPositions({
+    LatLng position1, LatLng position2
+  }) async {
+    return await _geolocator.distanceBetween(
+      position1.latitude, position1.longitude,
+      position2.latitude, position2.longitude,
     );
   }
 
-  Future<double> getDistanceBetweenPositions({LatLng position1, LatLng position2}) async {
+  Future<double> getDistanceBetweenAddresses({
+    String address1, String address2
+  }) async {
+
+    LatLng position1 = await getPositionFromAddress(address: address1);
+    LatLng position2 = await getPositionFromAddress(address: address2);
+
     return await _geolocator.distanceBetween(
-      position1.longitude, position1.latitude,
-      position2.longitude, position2.latitude,
+      position1.latitude, position1.longitude,
+      position2.latitude, position2.longitude,
     );
   }
 
