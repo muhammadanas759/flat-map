2fe93fcfdb16ffec7190568ba65bdf11fd6106a6
Bamkoff <bartek.fabisiak1@gmail.com>
2020-04-26 14:31:02 +0200

    Changed unit test for new markers_loader methods

--- test/resources/objects/data/markers_loader_test.dart
+++ test/resources/objects/data/markers_loader_test.dart
@@ -3,8 +3,9 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:flatmapp/resources/objects/data/markers_loader.dart';
 import 'package:preferences/preference_service.dart';
 
-void main() {
+Future<void> main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
+  await PrefService.init();
   group('Markters_Loader', () {
     test('Simple adding marker test', (){
       final markerLoader = MarkerLoader();
@@ -29,36 +30,35 @@ void main() {
       expect(markerLoader.markersDescriptions["test1"]["icon"], "default");
 
     });
-//    test('Simple removing marker test', () {
-//      TestWidgetsFlutterBinding.ensureInitialized();
-//      await PrefService.init();
-//      final markerLoader = MarkerLoader();
-//
-//      markerLoader.addMarker(
-//          id: "test1",
-//          position: LatLng(-43.0, 170.0),
-//          icon: "default",
-//          title: "test markter 1",
-//          description: "marker presenting chosen position 1",
-//          range: 12.5
-//      );
-//
-//      markerLoader.addMarker(
-//          id: "test2",
-//          position: LatLng(-73.0, 120.0),
-//          icon: "default",
-//          title: "test marker 2",
-//          description: "marker presenting chosen position 2",
-//          range: 11.5
-//      );
+    test('Simple removing marker test', () {
 
-//      markerLoader.removeMarker(
-//          id: "test1"
-//      );
-//
-//      expect(markerLoader.googleMarkers["test1"], null);
-//      expect(markerLoader.zones["test1"], null);
-//      });
+      final markerLoader = MarkerLoader();
+
+      markerLoader.addMarker(
+          id: "test1",
+          position: LatLng(-43.0, 170.0),
+          icon: "default",
+          title: "test markter 1",
+          description: "marker presenting chosen position 1",
+          range: 12.5
+      );
+
+      markerLoader.addMarker(
+          id: "test2",
+          position: LatLng(-73.0, 120.0),
+          icon: "default",
+          title: "test marker 2",
+          description: "marker presenting chosen position 2",
+          range: 11.5
+      );
+
+      markerLoader.removeMarker(
+          id: "test1"
+      );
+
+      expect(markerLoader.googleMarkers["test1"], null);
+      expect(markerLoader.zones["test1"], null);
+      });
 
     test('Simple editing markers test', (){
       final markerLoader = MarkerLoader();
